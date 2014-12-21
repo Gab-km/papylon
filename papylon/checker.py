3dc958205091e771d49bbbdcf3a6b0fd96734c21
Gab_km <the25thcromosome@gmail.com>
2014-12-22 06:51:18 +0900

    Add `such_that` method to `Gen` class in `papylon.gen`
    `CheckResult` is refactored and has new status `fail to generate`

--- papylon/checker.py
+++ papylon/checker.py
@@ -1,40 +1,57 @@
 # -*- coding: utf-8 -*-
 import sys
+from papylon.gen import StopGeneration
 from papylon.printer import SimplePrinter
 
 
 class CheckResult:
-    def __init__(self):
-        self.passed = None
-        self.falsified = None
-        self.troubled = None
+    PASSED = 0
+    FALSIFIED = 1
+    FAIL_TO_GENERATE = 2
+    TROUBLED = 3
+
+    def __init__(self, status):
+        self.status = status
+        self.result = None
 
     @staticmethod
     def pass_all(count):
-        result = CheckResult()
-        result.passed = (count,)
+        result = CheckResult(CheckResult.PASSED)
+        result.result = (count,)
         return result
 
     @staticmethod
     def falsify(run_count, inputs, error=None):
-        result = CheckResult()
-        result.falsified = (run_count, inputs, error)
+        result = CheckResult(CheckResult.FALSIFIED)
+        result.result = (run_count, inputs, error)
+        return result
+
+    @staticmethod
+    def fail_to_generate(run_count, trial_to_generate):
+        result = CheckResult(CheckResult.FAIL_TO_GENERATE)
+        result.result = (run_count, trial_to_generate)
         return result
 
     @staticmethod
     def trouble(error, ex_traceback):
-        result = CheckResult()
-        result.troubled = (error, ex_traceback)
+        result = CheckResult(CheckResult.TROUBLED)
+        result.result = (error, ex_traceback)
         return result
 
     def has_passed(self):
-        return self.passed is not None
+        return self.status == CheckResult.PASSED
 
     def has_falsified(self):
-        return self.falsified is not None
+        return self.status == CheckResult.FALSIFIED
+
+    def has_failed_to_generate(self):
+        return self.status == CheckResult.FAIL_TO_GENERATE
 
     def has_troubled(self):
-        return self.troubled is not None
+        return self.status == CheckResult.TROUBLED
+
+    def get(self):
+        return self.result
 
 
 class PropChecker:
@@ -54,16 +71,21 @@ class PropChecker:
                         return CheckResult.falsify(i+1, inputs)
                 else:
                     _, inputs, error = prop_result.stopped
-                    return CheckResult.falsify(i+1, inputs, error)
+
+                    # failed to generate?
+                    if type(error) == StopGeneration:
+                        return CheckResult.fail_to_generate(i+1, error.trial_to_generate)
+                    else:
+                        return CheckResult.falsify(i+1, inputs, error)
 
             return CheckResult.pass_all(self.count)
         except Exception as error:
             _, _, ex_traceback = sys.exc_info()
-            return CheckResult.trouble(error.args[0], ex_traceback)
+            return CheckResult.trouble(error, ex_traceback)
 
 
 def check(prop, count=100, printer_class=SimplePrinter):
     checker = PropChecker(count=count)
     result = checker.check(prop)
-    printer = printer_class()
-    printer.print_result(result)
+    printer = printer_class(result)
+    printer.print_result()
--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,43 +1,71 @@
 # -*- coding: utf-8 -*-
-from papylon.printer import SimplePrinter
-from papylon.checker import CheckResult
+def test_given_passed_result_when_print_result_then_report_ok(capsys):
+    from papylon.printer import SimplePrinter
+    from papylon.checker import CheckResult
 
-
-def test_given_passed_result_when_print_result_then_report_OK(capsys):
     result = CheckResult.pass_all(10)
-    sut = SimplePrinter()
-    sut.print_result(result)
-    out, _ = capsys.readouterr()
+    sut = SimplePrinter(result)
+    sut.print_result()
+    out, err = capsys.readouterr()
     assert out == "OK, passed 10 tests.\n"
+    assert err == ""
 
 
-def test_given_falsified_result_without_error_when_print_result_then_report_NOT_OK(capsys):
+def test_given_falsified_result_without_error_when_print_result_then_report_not_ok(capsys):
+    from papylon.printer import SimplePrinter
+    from papylon.checker import CheckResult
+
     result = CheckResult.falsify(5, ["test"])
-    sut = SimplePrinter()
-    sut.print_result(result)
-    out, _ = capsys.readouterr()
+    sut = SimplePrinter(result)
+    sut.print_result()
+    out, err = capsys.readouterr()
     assert out == "Falsified after 5 tests.\n> ['test']\n"
+    assert err == ""
 
 
-def test_given_falsified_result_with_error_when_print_result_then_report_NOT_OK_FOR_ERROR(capsys):
+def test_given_falsified_result_with_error_when_print_result_then_report_not_ok_for_error(capsys):
+    from papylon.printer import SimplePrinter
+    from papylon.checker import CheckResult
+
     result = CheckResult.falsify(6, ["ham", "egg"], ValueError("SPAM!"))
-    sut = SimplePrinter()
-    sut.print_result(result)
-    out, _ = capsys.readouterr()
+    sut = SimplePrinter(result)
+    sut.print_result()
+    out, err = capsys.readouterr()
     assert out == "Falsified after 6 tests.\n> ['ham', 'egg']\nwith exception:\nSPAM!\n"
+    assert err == ""
 
 
-def test_given_troubled_result_when_print_result_then_report_ERROR(capsys):
+def test_given_argument_failed_to_be_generated_result_when_print_result_then_report_failed_to_generate(capsys):
+    from papylon.printer import SimplePrinter
+    from papylon.checker import CheckResult
+
+    result = CheckResult.fail_to_generate(66, 100)
+    sut = SimplePrinter(result)
+    sut.print_result()
+    out, err = capsys.readouterr()
+    assert out == "Gave up after only 66 tests. 100 arguments failed to be generated.\n"
+    assert err == ""
+
+
+def test_given_troubled_result_when_print_result_then_report_error(capsys):
+    from papylon.printer import SimplePrinter
+    from papylon.checker import CheckResult
+
     result = CheckResult.trouble(ValueError("holy grail"), None)
-    sut = SimplePrinter()
-    sut.print_result(result)
-    _, err = capsys.readouterr()
+    sut = SimplePrinter(result)
+    sut.print_result()
+    out, err = capsys.readouterr()
+    assert out == ""
     assert err == "[Papylon] Some exception is raised.\nholy grail\n"
 
 
-def test_given_unknown_result_when_print_result_then_report_UNKNOWN(capsys):
-    result = CheckResult()
-    sut = SimplePrinter()
-    sut.print_result(result)
-    _, err = capsys.readouterr()
+def test_given_unknown_result_when_print_result_then_report_unknown(capsys):
+    from papylon.printer import SimplePrinter
+    from papylon.checker import CheckResult
+
+    result = CheckResult(-1)
+    sut = SimplePrinter(result)
+    sut.print_result()
+    out, err = capsys.readouterr()
+    assert out == ""
     assert err == "[Papylon] CheckResult doesn't have any known result types.\n"
